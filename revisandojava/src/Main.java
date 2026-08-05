75823b698d131a871664a16ba82cf73a125ac1b2
emanuelhmarcineksilva <emanuel.henriquemarcinek.silva@escola.pr.gov.br>
2026-08-05 11:56:39 -0300

    MISSAO-0: setup do trio - correcao

--- revisandojava/src/Main.java
+++ revisandojava/src/Main.java
@@ -1,8 +1,10 @@
+
+
 public class Main {
     public static void main(String[] args) {
-        SuperHeroi h = new Personagem();
+        SuperHeroi h = new SuperHeroi();
         h.nome = "Lisiane Reips";
-        h.sexo = 1;
+        h.sexo = Sexo.MULHER;
         h.altura = 1.75;
         h.identidadeSecreta = true;
         h.habilidades = new String[]{"super força", "visão de raio-x", "voar",
