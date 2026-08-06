6eb5442d8ab0648efe8d3e9d3485a2c27b7ae598
Gustavo <gustavo.dotplan@gmail.com>
2026-08-06 20:02:17 -0300

    MISSAO-3: heranca

---
+++ revisandojava/src/Vilao.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+public class Vilao extends Personagem {
+    private int nivelDeAmeaca;
+    public Vilao(String nome, Sexo sexo, double altura, int nivelDeAmeaca){
+        super(nome,sexo,altura);
+        if (nivelDeAmeaca <= 10 && nivelDeAmeaca >= 0) {
+            this.nivelDeAmeaca = nivelDeAmeaca;
+        }
+        else {
+            System.out.println("Nível de ameaça inválido!!");
+        }
+
+    }
+
+    @Override
+    public String apresentar() {
+        return "prazer, vilao";
+    }
+}
